215771809f4a6e1bee3d9b85ca4110b9d509d4ec
Halil İbrahim <77546808+yilmazhalilibr@users.noreply.github.com>
2021-01-23 18:16:41 +0300

    Add project files.

---
+++ MyDictionaryIntro/MyDictionary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryIntro
+{
+    class MyDictionary<T,T1>
+    {
+        public T[] DictKey;
+        public T[] TempDictKey;
+        public T1[] DictValue;
+        public T1[] TempDictValue;
+
+        public MyDictionary()
+        {
+            DictKey = new T[0];
+            DictValue = new T1[0];
+        }
+        public void Add(T keys , T1 values)
+        {
+            TempDictKey = DictKey;
+            TempDictValue = DictValue;
+
+            DictKey = new T[DictKey.Length + 1];
+            DictValue = new T1[DictValue.Length + 1];
+
+            for (int i = 0; i < TempDictKey.Length; i++)
+            {
+                TempDictKey[i] = DictKey[i];
+
+            }
+            for (int i = 0; i < TempDictValue.Length; i++)
+            {
+                TempDictValue[i] = DictValue[i];
+            }
+
+            DictKey[DictKey.Length - 1] = keys;
+            DictValue[DictValue.Length - 1] = values;
+
+
+
+        }
+
+
+    }
+}
---
+++ MyDictionaryIntro/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MyDictionaryIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> myDictionary = new MyDictionary<string, int>();
+
+            myDictionary.Add("Halil",25);
+            myDictionary.Add("Ahmet", 18);
+            myDictionary.Add("Engin", 35);
+            myDictionary.Add("Murat", 49);
+
+
+
+
+
+            Console.WriteLine(myDictionary.DictKey.Length);
+            Console.WriteLine(myDictionary.DictValue.Length);
+            Console.ReadLine();
+
+        }
+    }
+
+}
